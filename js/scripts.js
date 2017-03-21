32b59ac38e029502bfd260310a675d24a43479bc
jason welsh <jasonbwelsh@gmail.com>
2017-03-21 00:12:44 -0400

    Finished?
    Fixed the layout bug with the search feature. Need to test before
    submitting

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,13 @@ const search = document.querySelector('#search');
 const image = document.querySelectorAll('.image');
 let holdImg ; //holds reference to img each time through loop
 
-
 $('#search').on('keyup', () => {
   for (let i=0; i<image.length; i+=1) {
     holdImg = image[i];
-    /*console.log($(holdImg).attr('alt'));*/
-    if ( $(holdImg).attr('alt').includes(search.value) == true ) {
-      console.log('it worked');
-      holdImg.style.display = 'block';
+    if ( $(holdImg).attr('alt').includes(search.value.toLowerCase()) == true ) {
+      holdImg.parentElement.style.display = 'block';
     } else {
-      holdImg.style.display = 'none';
+      holdImg.parentElement.style.display = 'none';
     }
   }
 });
